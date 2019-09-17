1ba9da04afa3191c0235b392b595156f5b55b498
David Newham <davidpaulnewham@gmail.com>
2019-09-17 22:18:02 +0200

    add basic functionality to detect headset

--- main.c
+++ main.c
@@ -2,29 +2,48 @@
 #include "hidapi.h"
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
-  char device_handle = "name";
-  int r = 0;
-  printf("requesting if active\n");
-  unsigned char data_request[58] = {0x1c, 0x00, 0xb0, 0x9a, 0x19, 0xb8, 0x8d, 0xaf, 0xff, 0xff, 0, 0, 0, 0, 0x08, 0, 0x01, 0x05, 0x00, 0x01, 0, 0, 0x02, 0x1f, 0, 0, 0, 0x01, 0x06, 0x18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  
-  r = hid_write(device_handle, data_request, 2);
-  if (r < 0) return r;
-  
-  unsigned char data_read[5];
-   
-   
-	printf("hello world\n");
-   
-  
-  
-  return 0;
+int is_device_connected(hid_device *device_handle);
+int find_usb_devices();
+
+int main(int argc, char **argv) {
+
+    while (1) {
+        //TODO: Investigate threads
+        sleep(10);
+        char device_handle = "name";
+        if (is_device_connected(device_handle) == 1){
+            printf("Arctis 7 headset connected");
+            //set default device to headset
+        }
+        else{
+            printf("Could not find headset");
+            //set other default output
+        }
+    }
+    return 0;
 }
 
-int	find_usb_devices()
-{
-	struct device;
-	int found;
+int is_device_connected(hid_device *device_handle) {
+    int r = 0;
+    int connected = 0;
+
+    // thanks to the reverse engineering from https://github.com/Sapd/HeadsetControl
+    // send request for battery status
+    unsigned char data_request[2] = {0x06, 0x18};
+    r = hid_write(device_handle, data_request, 2);
+
+    // read the result
+    unsigned char data_read[8];
+    r = hid_read(device_handle, data_read, 8);
+    if (r < 0) return 0;
+    int bat = data_read[2];
+    if (bat > 0) return 1;
+}
+
+
+int find_usb_devices() {
+    struct device;
+    int found;
 }
